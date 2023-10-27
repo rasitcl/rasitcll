c12c05cf90369573a4904fde58e780945812bf90
rasitcl <148677534+rasitcl@users.noreply.github.com>
2023-10-27 17:09:37 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,94 @@
-package Java_Dersleri1;
+class Animal {
 
-import java.util.Scanner;
+    private String name;
 
-public class Main {
-    public static void mean(int[] arr){
-        int total = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            total+= arr[i];
-        }
-        System.out.println("Ortalama: "+(double)total/arr.length);
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] arr2 = new int[5];
-
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = scanner.nextInt();
-        }
-    System.out.println( "Array2'nin değerleri yazılıyor...");
-
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.println("Eleman: "+ arr2[i]);
-        }
-
-mean(arr2);
-
-
-
+    public Animal(String name) {
+        this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+
+        System.out.println("Hayvan konuşuyor...");
+    }
+}
+
+class Bird extends Animal {
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void speak() {
+
+        System.out.println(getName() + " ötüyor...");
+    }
+
+
+    static class Dog extends Animal {
+        public Dog(String name) {
+            super(name);
+        }
+
+
+        @Override
+        public void speak() {
+
+            System.out.println(getName() + " havlıyor...");
+        }
+
+
+        static class Horse extends Animal {
+            public Horse(String name) {
+                super(name);
+            }
+
+
+            @Override
+            public void speak() {
+
+                System.out.println(getName() + " kişniyor...");
+            }
+        }
+
+        public static class Main {
+
+            public static void speaktry(Animal animal) {
+
+                animal.speak();
+            }
+
+            public static void main(String[] args) {
+
+                Animal animal = new Animal("Hayvan");
+
+                Bird bird = new Bird("Limon");
+
+                Dog dog = new Dog("Karabaş");
+
+                Horse horse = new Horse("Şahbatur");
+
+              /*  animal.speak();
+
+                bird.speak();
+
+                dog.speak();
+
+                horse.speak();*/
+
+               speaktry(new Bird("Limon"));
+
+
+
+
+            }
+        }
+    }
 }
